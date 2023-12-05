a5d96c69afb4e0ff7867f9f114e692538a5fb1bd
Saranorm <saranorm@kth.se>
2023-12-05 19:51:06 +0100

    InformationView and presenter

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
+
 import {createApp} from "vue";
-function App(){ return <div>hello world</div>; };
+function App(){ return <div>Hello World!</div>; };
 const app= createApp(App);
 app.mount("#root");
--- src/presenter/informationPresenter.jsx
+++ src/presenter/informationPresenter.jsx
@@ -1,29 +1,14 @@
-import InformationView from "informationView.jsx";
+import InformationView from "../views/informationView.jsx";
 
 //model is starModel
 export default function Information(props){
     function visitedCB(location){return location === props.model.currentLocation;}
-    function addToWantToGoACB(){props.model.addToMenu(props.model.currentLocationPromiseState.data);}
-    function addVisitedACB(){props.model.addToMenu(props.model.currentLocationPromiseState.data);}
+    function addToWantToGoACB(){props.model.addToMenu();}
+    function addVisitedACB(){props.model.addToMenu();}
 
-    function promiseNoData(promiseState){
-        if(!promiseState.promise){//if promiseState.promise is false, no data should be returned.
-            return "no data";
-        }  
-        else{ // if promise is true, check data and error. 
-            if(!promiseState.data){
-                if(!promiseState.error){return <img src="https://brfenergi.se/iprog/loading.gif"/>;}
-                return promiseState.error.toString(); //if promiseState.error is true and promise.data is false return error 
-            }
-            return false; //if promiseState.data is true (and promiseState.promise is true) return view
-        }
-    }
-
-    return (promiseNoData(props.model.currentLocationPromiseState) || 
-    (<InformationView 
-        locationData={props.model.currentLocationPromiseState.data} 
+    return (<InformationView 
+        locationData={props.model} 
         isLocVisited={props.model.haveVisited.find(visitedCB)} //isLocVisited expected to be falsy with empty menu
         WantToGoACB={addToWantToGoACB}
-        VisitedACB={addVisitedACB}/>) //add astroEvents, moonphase 
-        );
+        VisitedACB={addVisitedACB}/>); //add astroEvents, moonphase 
 }
