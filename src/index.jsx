535bed417c44907062f24a45ca3d8b6202087530
jolinefrisk <jolinef@kth.se>
2023-12-06 09:45:17 +0100

    Added to index
    took info from tutorial weeks and added it to our project with minor changes. when we are ready to start working with it we can start un commenting things

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,36 @@
 
-import {createApp} from "vue";
-function App(){ return <div>Hello World!</div>; };
+import "/src/teacherFetch.js"; // protection against fetch() in infinite re-render
+
+//import {makeRouter,Root} from "./root.jsx"; 
+import "/src/firebaseModel.js";
+//import connectToFirebase from "../firebaseModel.js";
+//import model from "/src/starModel.js";
+
+
+import { reactive, watch,createApp,h } from "vue";
+
+/*const reactiveModel= reactive(model);
+
+window.React= {createElement:h};
+const app= createApp(<Root model={reactiveModel}/>);
+
+app.use(makeRouter(reactiveModel));
+app.mount('#root'); 
+
+
+reactiveModel.doSearch({});
+connectToFirebase(reactiveModel, watch);
+
+window.myModel= reactiveModel;  */
+
+
+import Login from "./presenter/loginPresenter.jsx";
+
+
+function App(){ return <div><Login/></div>; };
 const app= createApp(App);
 app.mount("#root");
+
+
+
+
