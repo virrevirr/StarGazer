326fe560d32369f3f07c311177e7997fe56ae4c9
jolinefrisk <jolinef@kth.se>
2023-12-08 13:24:25 +0100

    Update starSource.js

--- src/starSource.js
+++ src/starSource.js
@@ -1,16 +1,16 @@
 //funkar som dishSource.js
 
-import {API_KEY} from "./apiConfig.js";
+import { API_KEY} from "./apiConfig.js";
 
 //API search for places
 export function searchPlaces(searchParams){
     const queryPlace = new URLSearchParams({"query":searchParams, "searchby":"city"}).toString(); //blir rätt format nu
-    const source = "andruxnet-world-cities-v1.p.rapidapi.com/?" + queryPlace;
+    const source = "https://andruxnet-world-cities-v1.p.rapidapi.com/?" + queryPlace;
     
     return fetch(source, {
         method:"GET", 
         headers: {'X-Mashape-Key': API_KEY}
-    }).then(getJsonACB).then(keepArrayACB)
+    }).then(getJsonACB).then(keepArrayACB);
 }
 
 function getJsonACB(resp){
@@ -20,9 +20,7 @@ function getJsonACB(resp){
     console.log(resp);
     return resp.text();
 }
-
 function keepArrayACB(searchInput){ 
+    console.log(searchInput);
     return searchInput.results;
 }
-
-
