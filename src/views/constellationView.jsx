c9861a2ee0f1ab88bc0184c98637b3133f16f225
virao <virao@kth.se>
2023-12-12 16:58:01 +0100

    constellation views added

---
+++ src/views/constellationView.jsx
@@ -0,0 +1,28 @@
+// försök på stjärnbilder som inte funkat så bra
+
+function StarView(props){
+    console.log("stars",props.starData[0].constellation)
+
+    function starsInConstellation(){
+        for (let i = 0; i < props.starData.length; i++) {
+            return props.starData.constellation[i]
+        }
+    }
+
+    return (
+        <div className="mainContainer">
+            <h1>{props.starData.constellation}</h1> 
+
+            <div>
+                <img src = {"https://en.wikipedia.org/wiki/"+props.starData[0].constellation+
+                "_(constellation)#/media/File:"+props.starData.constellation[0]+"_IAU.svg"} height = {"200"} />
+                
+                <h3>Stars included</h3> 
+                <h4>{starsInConstellation}</h4> 
+            </div>
+        
+        </div>
+    );
+}
+
+export default StarView;
