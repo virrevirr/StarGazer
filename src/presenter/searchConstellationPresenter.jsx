449c9c0a8b0746664be1d6077d28b181ac511f94
virao <virao@kth.se>
2023-12-13 14:25:10 +0100

    constellation things

--- src/presenter/searchConstellationPresenter.jsx
+++ src/presenter/searchConstellationPresenter.jsx
@@ -1,3 +1,65 @@
 // Search presenter för stjärnbilderna, ska likna searchPresenter.jsx
 // importera listan från constellations.jsx
 // skicka listan till searchConstellationView.jsx
+
+import SearchFormView from "../views/searchFormView";
+import SearchResultsView from "../views/searchResultsView";
+import countries from "/src/countries.jsx"
+
+export default function Constellation(props){
+
+    function setSearchInputACB(newInput){
+        props.model.setSearch(newInput);
+    }
+
+    function searchNowACB(){
+        props.model.startSearch(props.model.searchParams);
+    }
+
+    function onLocationClickACB(input){
+        props.model.setCurrentLocation(input);
+
+        // gör även i profile on location click
+        props.model.searchWeatherByCity(input.city);
+        props.model.getMoon();
+
+        const countryToCode = countries[input.country].alpha2;
+        const languageToCode = countries[input.country].iso6391;
+        console.log("Country code", countryToCode);
+        console.log("Language code", languageToCode);
+        props.model.searchNewsByCountry(languageToCode, countryToCode);
+        //props.model.searchConstellation();
+    }
+
+    function promiseData(promiseState){
+
+        {/* Code with api fetch */}
+        if(!promiseState.promise){//if promiseState.promise is false, no data should be returned.
+            return "no data";
+        }  
+        else{ // if promise is true, check data and error. 
+            if(!promiseState.data){
+                if(!promiseState.error){return <img src="https://brfenergi.se/iprog/loading.gif"/>;}
+                return promiseState.error.toString(); //if promiseState.error is true and promise.data is false return error 
+            }
+            return <SearchResultsView className="in promise state" searchResult = {props.model.searchResultsPromiseState.data}
+            onLocationClick = {onLocationClickACB}/>;
+        }
+
+        {/* Test code without api fetch */}
+        /*return <SearchResultsView
+            onLocationClick = {onLocationClickACB}
+             />;*/
+    }
+
+    return (
+        <div>
+            <SearchFormView
+            text = {props.model.searchParams}
+            userInput= {setSearchInputACB} //setSearch from model
+            clickSearch= {searchNowACB} //startSearch from model (makes a promise) 
+/> {promiseData(props.model.searchResultsPromiseState)}
+            
+        </div>
+        );
+}
