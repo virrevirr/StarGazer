328e408ec0f1dedfde9df8580d58bb41bdd6a741
virao <virao@kth.se>
2023-12-13 10:33:02 +0100

    Update informationView.jsx

--- src/views/informationView.jsx
+++ src/views/informationView.jsx
@@ -4,10 +4,10 @@ function InformationView(props){
     console.log("props.weatherData", props.weatherData)
     console.log("props.locationData", props.locationData)
 
-    function WantToGoACB(event){
+    function wantToGoACB(event){
         return props.addToGo(props.locationData);}
     
-    function VisitedACB(event){
+    function visitedACB(event){
         return props.addToVisited(props.locationData);}
 
     function cancelACB(){console.log(window.location.hash="#/search");}
@@ -18,8 +18,8 @@ function InformationView(props){
             <span>
                 <h1>{props.locationData.city +", "+ props.locationData.country}</h1> 
                 <div>
-                    <button onClick={WantToGoACB} disabled={props.isLocVisited}>I want to go here</button>
-                    <button onClick={VisitedACB}>I have been here</button>
+                    <button onClick={wantToGoACB} disabled={props.isLocVisited}>I want to go here</button>
+                    <button onClick={visitedACB}>I have been here</button>
                 </div>
                 {/*MoonPhase - figure out how to do this*/} 
             </span>
