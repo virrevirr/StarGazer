a900eff35c03305b56f995541b79d45d472a22cb
jolinefrisk <jolinef@kth.se>
2023-12-18 10:16:41 +0100

    re do

--- src/views/loginView.jsx
+++ src/views/loginView.jsx
@@ -1,22 +1,11 @@
 /* code snippets taken from https://clerk.com/blog/building-a-react-login-page-template */
 import "../style.css";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+ 
 
 function LogInView(props){
-  
     function authGoogleACB() {
-        const auth = getAuth(app);
-      const provider = new GoogleAuthProvider();
-
-      signInWithPopup(auth, provider)
-        .then((result) => {
-          const user = result.user;
-          console.log("User signed in with Google:", user);
-          // You can redirect or perform additional actions here
-        })
-        .catch((error) => {
-          console.error("Google authentication failed:", error.message);
-        });
+        props.authenticator(auth, provider);
       }
 
     return(
