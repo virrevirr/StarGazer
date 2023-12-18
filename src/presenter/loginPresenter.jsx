3a255330646fe607ff9e7e45dbdd7c5ea0622689
jolinefrisk <jolinef@kth.se>
2023-12-18 15:21:07 +0100

    working login

--- src/presenter/loginPresenter.jsx
+++ src/presenter/loginPresenter.jsx
@@ -4,20 +4,18 @@
 
 import LogInView from "../views/loginView.jsx";
 import { auth } from "../firebaseModel";
-import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup,  signOut } from "firebase/auth";
 
 function Login(props) {
   const provider = new GoogleAuthProvider();
 
-  function connect() {
+async function connect() {
     console.log("loginAB");
     try {
-      auth.currentUser?(signOut(auth)): (signInWithPopup(auth, provider))
-      
+      auth.currentUser? await signOut(auth): (await signInWithPopup(auth, provider),window.location.hash = '#/search');
     } catch (error) {
       console.error("Error during login/logout:", error);
     }
-    window.location.hash = '#/search';
   }
 
   return <LogInView loginCB={connect} />;
