bdfa500b5cea88c646a0968b6d9821d599ab8b1d
Saranorm <saranorm@kth.se>
2023-12-18 13:48:30 +0100

    infoView nice layout changes

--- src/views/newsView.jsx
+++ src/views/newsView.jsx
@@ -19,7 +19,7 @@ function NewsView(props){
     return (
         <div className="localNews"> {/* Lägg till class för att rendera news som prototypen*/}
             <h2> Local astronomical news </h2>
-            <div>  {/* Lägg till class för att rendera dom 3 nyheterna bredvid varandra i kolumner (typ som grid) */}
+            <div className="newsScroll">  {/* Lägg till class för att rendera dom 3 nyheterna bredvid varandra i kolumner (typ som grid) */}
                 {news()}
             </div>
         </div>
