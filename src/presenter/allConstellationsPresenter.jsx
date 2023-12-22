6c3b67dc967625abd9475654c30699048c433bc4
virao <virao@kth.se>
2023-12-22 18:25:07 +0100

    allconstellations commented

--- src/presenter/allConstellationsPresenter.jsx
+++ src/presenter/allConstellationsPresenter.jsx
@@ -1,12 +1,10 @@
 import AllConstellationsView from "../views/allConstellationsView.jsx";
-import starConstellations from "../starConstellations.jsx"
+import starConstellations from "../starConstellations.jsx" // Importing constellation dictionary
 import { connect } from "./loginPresenter";
 
 export default function AllConstellations(props){
-
     function onConstellationClickACB(constellation){
-        /*Fetches the stars included in the chosen constellation*/
-        /*props.model.fetchConstellation(props.model.currentConstellation);*/
+        // Fetches the stars included in the clicked constellation
         props.model.setCurrentConstellation(constellation);
     }
 
--- src/views/allConstellationsView.jsx
+++ src/views/allConstellationsView.jsx
@@ -1,35 +1,42 @@
 import {AccountOutlineIcon, LogoutIcon} from "/src/components.jsx";
 
 function AllConstellationsView(props){
-    /*console.log("all constellations", props.allConstellations)*/
+    // View to render all constellations the user can choose in a grid
 
     function navigateToProfileACB(){
+        // ACB to navigate to profile
         window.location.hash = '#/profile';
     }
     
     function navigateToLoginACB(){
+        // ACB to navigate to login
         props.loginCB();
     };
 
     function navigateToInformationACB(){
+        // ACB to navigate to information
         window.location.hash = '#/information';
     };
 
     return(
     <div>
         <div className="backToSearchButton">
+            {/* Button to send user back to search */}
             <button className="biggerButtonDesign" onClick={navigateToInformationACB}>Back to {props.location.city}</button>
         </div>
 
         <div className="profileButton">
+            {/* Button to send user to profile */}
             <AccountOutlineIcon title="Profile" onClick={navigateToProfileACB} fillColor="#ffffff" size="48"/>
         </div>
         
         <div class = "ParentContainer constellationsContainer">
+            {/* Container to render all constellations */}
             {Object.entries(props.allConstellations).map(renderResultCB)}
         </div> 
 
         <div className="logOutButton">
+            {/* Button to log out user */}
             <LogoutIcon title="Log Out" onClick={navigateToLoginACB} fillColor="#ffffff" size="48"/>
         </div>
 
@@ -37,15 +44,16 @@ function AllConstellationsView(props){
     )
 
     function renderResultCB(constellation){
-        // Renderar bild och text på varje constellation
+        // Renders image and text for each constellation
         function onConstellationClickACB(event){
-            /*contellation[0] is the name of the constellation*/
+            // Callback to navigate to chosen constellation
             props.onConstellationClick(constellation[0]);
             window.location.hash= "#/constellation";
         };
 
         return(
             <div className="constellationDisplay">
+                {/* Constellation[0] is the name of the constellation, constellation[1] is the url to the image */}
                 <span key={constellation[0]} onClick={onConstellationClickACB}> 
                     <img src = {constellation[1]} height = {"100"} />
                     <p>{constellation[0]}</p>
