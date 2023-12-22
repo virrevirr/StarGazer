a3a8292b84e511acdbbda0b8a96234bf9c808587
virao <virao@kth.se>
2023-12-22 18:36:00 +0100

    Update constellationPresenter.jsx

--- src/presenter/constellationPresenter.jsx
+++ src/presenter/constellationPresenter.jsx
@@ -1,30 +1,33 @@
 import ConstellationView from "../views/constellationView.jsx";
 import { connect } from "./loginPresenter";
-export default function Constellation(props){
-    
-    function haveSeenACB(constellation){
-        // Add to seen
-        props.model.addToSeen(props.model.currentLocation, constellation);
-    }
+export default function Constellation(props) {
+  function haveSeenACB(constellation) {
+    // Add to seen
+    props.model.addToSeen(props.model.currentLocation, constellation);
+  }
 
-    function promiseData(promiseState){
-        // Function to handle promiseState
-        if(!promiseState.promise){
-            return <p>no data</p>; 
-        }  
-        if(!promiseState.data){
-            if(!promiseState.error){
-                return <img src="https://brfenergi.se/iprog/loading.gif" height = {"50"}/>;
-            }
-            return <p>{promiseState.error.toString()}</p>;
-        }
-        return <ConstellationView 
-        currentConstellation = {promiseState.data}
-        haveSeen = {haveSeenACB} 
-        loginCB ={connect}
-        location = {props.model.currentLocation}
-        />;
+  function promiseData(promiseState) {
+    // Function to handle promiseState
+    if (!promiseState.promise) {
+      return <p>no data</p>;
     }
+    if (!promiseState.data) {
+      if (!promiseState.error) {
+        return (
+          <img src="https://brfenergi.se/iprog/loading.gif" height={"50"} />
+        );
+      }
+      return <p>{promiseState.error.toString()}</p>;
+    }
+    return (
+      <ConstellationView
+        currentConstellation={promiseState.data}
+        haveSeen={haveSeenACB}
+        loginCB={connect}
+        location={props.model.currentLocation}
+      />
+    );
+  }
 
-    return (promiseData(props.model.constellationPromiseState));
+  return promiseData(props.model.constellationPromiseState);
 }
