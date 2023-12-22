7b9813b404c1a944ece5543541db7f62748e9914
Saranorm <saranorm@kth.se>
2023-12-22 18:30:14 +0100

    Formatting information

--- src/presenter/informationPresenter.jsx
+++ src/presenter/informationPresenter.jsx
@@ -1,4 +1,3 @@
-
 //Neccesary imports
 import InformationView from "../views/informationView.jsx";
 import WeatherView from "../views/weatherView.jsx";
@@ -6,59 +5,63 @@ import MoonView from "../views/moonView.jsx";
 import NewsView from "../views/newsView.jsx";
 import { connect } from "./loginPresenter";
 
-export default function Information(props){
-    function addToWantToGoACB(){
-        /*Adds chosen location to wantToGo arry in starModel*/
-        props.model.addToWantToGo(props.model.currentLocation);
+export default function Information(props) {
+  function addToWantToGoACB() {
+    /*Adds chosen location to wantToGo arry in starModel*/
+    props.model.addToWantToGo(props.model.currentLocation);
+  }
+  function addVisitedACB() {
+    /*Adds chosen location to haveVisited arry in starModel*/
+    props.model.addToVisited(props.model.currentLocation);
+  }
+
+  function inVisitedCB(location) {
+    /*Checks if the current location is already saved in an array by comparison*/
+    return location.city === props.model.currentLocation.city;
+  }
+
+  function promiseData(promiseState) {
+    /*Will check the promisteState of several promises and only render the views if data is available*/
+    if (!promiseState.promise) {
+      //if promiseState.promise is false, no data should be returned.
+      return <p>no data</p>;
     }
-    function addVisitedACB(){
-        /*Adds chosen location to haveVisited arry in starModel*/
-        props.model.addToVisited(props.model.currentLocation);
+    // if promise is true, check data and error.
+    if (!promiseState.data) {
+      if (!promiseState.error) {
+        return (
+          <img src="https://brfenergi.se/iprog/loading.gif" height={"50"} />
+        );
+      }
+      return <p>{promiseState.error.toString()}</p>; //if promiseState.error is true and promise.data is false return error
     }
-    
-    function inVisitedCB(location){
-        /*Checks if the current location is already saved in an array by comparison*/
-        return location.city === props.model.currentLocation.city;
+    if (promiseState.data.moon) {
+      return <MoonView moonData={promiseState.data} />;
     }
 
-    function promiseData(promiseState){
-        /*Will check the promisteState of several promises and only render the views if data is available*/
-        if(!promiseState.promise) { //if promiseState.promise is false, no data should be returned.
-            return <p>no data</p>;
-        }  
-        // if promise is true, check data and error. 
-        if(!promiseState.data){
-            if(!promiseState.error){
-                return <img src="https://brfenergi.se/iprog/loading.gif" height = {"50"}/>;
-            }
-            return <p>{promiseState.error.toString()}</p>; //if promiseState.error is true and promise.data is false return error 
-        }
-        if(promiseState.data.moon){
-            return (<MoonView moonData = {promiseState.data}/>);
-        }
-
-        if(promiseState.data.forecast){
-            return  (<WeatherView weatherData = {promiseState.data}/>);
-        }
-        
-        return (<NewsView newsData = {promiseState.data}/>);
+    if (promiseState.data.forecast) {
+      return <WeatherView weatherData={promiseState.data} />;
     }
 
-    return (
-        <div>
-        <InformationView 
+    return <NewsView newsData={promiseState.data} />;
+  }
+
+  return (
+    <div>
+      <InformationView
         locationData={props.model.currentLocation}
-        addToGo ={addToWantToGoACB}
+        addToGo={addToWantToGoACB}
         addToVisited={addVisitedACB}
         locvisited={props.model.haveVisited.some(inVisitedCB)}
-        locgo = {props.model.wantToGo.some(inVisitedCB)}
-        loginCB ={connect}/>
-            <div className="ParentContainer">
-                {/*The below will be rendered alongside eachother*/}
-                {promiseData(props.model.weatherPromiseState)}
-                {promiseData(props.model.moonPromiseState)}
-                {promiseData(props.model.newsPromiseState)}
-            </div>
-        </div>
-        );
+        locgo={props.model.wantToGo.some(inVisitedCB)}
+        loginCB={connect}
+      />
+      <div className="ParentContainer">
+        {/*The below will be rendered alongside eachother*/}
+        {promiseData(props.model.weatherPromiseState)}
+        {promiseData(props.model.moonPromiseState)}
+        {promiseData(props.model.newsPromiseState)}
+      </div>
+    </div>
+  );
 }
