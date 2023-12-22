6417c8f31bf66621f019c255ae2ae17eb6febd6e
Saranorm <saranorm@kth.se>
2023-12-22 17:39:39 +0100

    Update root.jsx

--- src/root.jsx
+++ src/root.jsx
@@ -1,3 +1,4 @@
+//Neccesary imports
 import Login from "./presenter/loginPresenter.jsx";
 import Profile from "./presenter/profilePresenter.jsx";
 import Search from "./presenter/searchPresenter.jsx";
@@ -9,21 +10,17 @@ import { createRouter, createWebHashHistory, RouterView} from "vue-router";
 
 export default function VueRoot(props){
     function modelReady(){
-        console.log("root model ready: ", props.model.ready)
+        //Will check if model is ready, and only render RouterView if it is, otherwise suspense.
         return props.model.ready? false : <img src="https://brfenergi.se/iprog/loading.gif"/>;
     }
     return (modelReady() || (<div className="flexParent">
                             <div className="mainContent"><RouterView/></div>
                             </div>)
             );
-
-    /*Bypass the props.model.ready*/
-    /*return (<div className="flexParent">
-            <div className="mainContent"><RouterView/></div>
-            </div>);*/
 }
 
 export function makeRouter(model){
+    //Creates the routes in RouterView and sets the model for each presenter to starModel.
     return createRouter({
         history: createWebHashHistory(),
         routes: [
