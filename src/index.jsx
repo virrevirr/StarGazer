23c7709cee5635a2f4bb8a35fe737fecfcb4a14d
lovisaconde <lconde@kth.se>
2023-12-22 18:40:02 +0100

    fomattering constellationView

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,26 @@
-import {makeRouter} from "./root.jsx"; 
+import { makeRouter } from "./root.jsx";
 
 // Application state (model)
 import "/src/firebaseModel.js";
 import connectToFirebase from "./firebaseModel.js";
 import model from "/src/starModel.js";
 
-// Make the app update when the model changes, and mounting 
+// Make the app update when the model changes, and mounting
 // the root component in the browser page.
 import { reactive, watch, createApp, h } from "vue";
 
-const reactiveModel= reactive(model);
-window.React= {createElement:h};
+const reactiveModel = reactive(model);
+window.React = { createElement: h };
 
 import VueRoot from "./root.jsx";
-const app= createApp(<VueRoot model={reactiveModel}/>);
+const app = createApp(<VueRoot model={reactiveModel} />);
 
 app.use(makeRouter(reactiveModel));
-app.mount('#root'); 
-
+app.mount("#root");
 
 // Connecting the model to Firebase
 connectToFirebase(reactiveModel, watch);
-window.myModel= reactiveModel; 
+window.myModel = reactiveModel;
 
- // Shows some initial cities
- reactiveModel.startSearch("paris");
+// Shows some initial cities
+reactiveModel.startSearch("paris");
